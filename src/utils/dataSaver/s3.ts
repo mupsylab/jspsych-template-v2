85843d79e01c1ea2e4d8cb0f5ae30b7e17a13417
mupsy <amm@mupsy.net>
2025-08-03 21:58:22 +0800

    use presign url, then allow second path on URL

--- src/utils/dataSaver/s3.ts
+++ src/utils/dataSaver/s3.ts
@@ -1,4 +1,5 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export function save_s3(opts: {
     csv: string,
@@ -11,8 +12,9 @@ export function save_s3(opts: {
 }) {
     return new Promise((resolve, reject) => {
         const { csv, accessKey, secretKey, bucket, endpoint, region, fileName } = opts;
+        const origin = new URL(endpoint).origin;
         const s3 = new S3Client({
-            endpoint, region,
+            endpoint: origin, region,
             credentials: {
                 accessKeyId: accessKey,
                 secretAccessKey: secretKey
@@ -20,14 +22,22 @@ export function save_s3(opts: {
             forcePathStyle: true
         });
 
-        s3.send(
+        getSignedUrl(
+            s3, 
             new PutObjectCommand({
                 Bucket: bucket,
                 Key: fileName,
-                Body: csv
+                ContentType: 'text/csv'
+            }),
+            { expiresIn: 600 }
+        ).then(url => {
+            const u = url.replace(origin, endpoint);
+            fetch(u, {
+                method: 'PUT',
+                body: csv
             })
-        )
-        .then(() => resolve)
-        .catch(reject);
+            .then(resolve)
+            .catch(reject);
+        }).catch(reject);
     });
 }
